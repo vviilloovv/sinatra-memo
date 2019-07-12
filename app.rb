ec9b4615c90901b8b7c4bb5a3148e3eb934048d4
vviilloovv <vviilloovv@gmail.com>
2019-07-12 16:19:23 +0900

    add show/edit page

--- app.rb
+++ app.rb
@@ -1,18 +1,33 @@
 require "sinatra"
 require "sinatra/reloader"
+require "json"
+
+json_file_path = "./public/memos.json"
 
 get "/" do
   @page_title = "Top"
+  @memos = open(json_file_path) { |io| JSON.load(io) }["memos"]
   erb :index
 end
 
-get "/create" do
+get "/new" do
   @page_title = "New memo"
-  erb :create
+  erb :new
 end
 
-post "/create" do
+post "/new" do
 
-  erb :create
+  erb :new
+end
+
+get "/show" do
+  @page_title = "Show memo"
+  @memo = params[:memo]
+  erb :show
+end
+
+get "/edit" do
+  @page_title = "Edit memo"
+  erb :edit
 end
 
